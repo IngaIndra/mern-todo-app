83f28a84938d816fe2ad98396405baa9bc82484f
Indra <indra.ulziibadrakh@gmail.com>
2023-10-24 09:47:44 +0800

    added Todo to DB

--- todo-frontend/src/components/Todo/index.js
+++ todo-frontend/src/components/Todo/index.js
@@ -6,7 +6,6 @@ import Form from '../Form';
 function Todo() {
     const [input, setInput] = useState('');
     const [todos, setTodos] = useState([])
-    // console.log(input, 'input');
 
     const fetchData = async () => {
         try {
@@ -21,8 +20,16 @@ function Todo() {
         fetchData()
     }, []);
 
-    const addTodo = async () => {
-        console.log('addedTodo')
+    const addTodo = async (e) => {
+        e.preventDefault();
+        if (input.length === 0) return null;
+        await axios.post('/todos', [{
+            ...todos,
+            text: input,
+            completed: false
+        }])
+        fetchData();
+        setInput('');
     }
 
     console.log(todos, 'todos');
@@ -32,7 +39,7 @@ function Todo() {
         <Container>
             <h2>List of Todos</h2>
             {/* Form component */}
-            <Form input={input} setInput={setInput} />
+            <Form input={input} setInput={setInput} addTodo={addTodo} />
             {/* Todo list component*/}
             {/* Key component*/}
             {/* Author component */}
